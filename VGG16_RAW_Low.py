d0ad0bd80b59652ae347c6d72fb126993b7c1535
MuneebKhan46 <81796045+MuneebKhan46@users.noreply.github.com>
2024-07-16 03:36:10 +0900

    Update and rename VGG_RGB.py to VGG16_RAW_Low.py

--- VGG16_RAW_Low.py
+++ VGG16_RAW_Low.py
@@ -4,7 +4,6 @@ import os
 from os import path
 import csv
 import textwrap
-from PIL import Image
 import pandas as pd
 
 from tensorflow.keras.regularizers import l1
@@ -31,31 +30,34 @@ class_1_accuracies = []
 original_dir = '/WACV_Paper/Dataset/maid-dataset-high-frequency/original'
 denoised_dir = '/WACV_Paper/Dataset/maid-dataset-high-frequency/denoised'
 csv_path     = '/WACV_Paper/Dataset/maid-dataset-high-frequency/classified_label.csv'
-result_file_path = "/WACV_Paper/Result/RGB_Result_High_Frequency.csv"
+
+result_file_path = "/WACV_Paper/Result/LOW_Frequency_RAW_Results.csv"
 
 #########################################################################################################################################################################################################################################
 
-def extract_patches_from_rgb_image(image_path: str, patch_size: int = 224):
+def extract_y_channel_from_yuv_with_patch_numbers(yuv_file_path: str, width: int, height: int):
+    y_size = width * height
     patches, patch_numbers = [], []
-    
-    if not os.path.exists(image_path):
-        print(f"Warning: File {image_path} does not exist.")
+
+    if not os.path.exists(yuv_file_path):
+        print(f"Warning: File {yuv_file_path} does not exist.")
         return [], []
 
-    image = Image.open(image_path)
-    if image.mode != 'RGB':
-        print(f"Warning: Expected an RGB image, got {image.mode}.")
+    with open(yuv_file_path, 'rb') as f:
+        y_data = f.read(y_size)
+
+    if len(y_data) != y_size:
+        print(f"Warning: Expected {y_size} bytes, got {len(y_data)} bytes.")
         return [], []
 
-    width, height = image.size
-    image_array = np.array(image)
+    y_channel = np.frombuffer(y_data, dtype=np.uint8).reshape((height, width))
     patch_number = 0
 
-    for i in range(0, height, patch_size):
-        for j in range(0, width, patch_size):
-            patch = image_array[i:i+patch_size, j:j+patch_size]
-            if patch.shape[0] < patch_size or patch.shape[1] < patch_size:
-                patch = np.pad(patch, ((0, patch_size - patch.shape[0]), (0, patch_size - patch.shape[1]), (0, 0)), 'constant')
+    for i in range(0, height, 224):
+        for j in range(0, width, 224):
+            patch = y_channel[i:i+224, j:j+224]
+            if patch.shape[0] < 224 or patch.shape[1] < 224:
+                patch = np.pad(patch, ((0, 224 - patch.shape[0]), (0, 224 - patch.shape[1])), 'constant')
             patches.append(patch)
             patch_numbers.append(patch_number)
             patch_number += 1
@@ -75,14 +77,14 @@ def load_data_from_csv(csv_path, original_dir, denoised_dir):
 
     for _, row in df.iterrows():
         
-        original_file_name = f"original_{row['image_name']}.png"
-        denoised_file_name = f"denoised_{row['image_name']}.png"
+        original_file_name = f"original_{row['image_name']}.raw"
+        denoised_file_name = f"denoised_{row['image_name']}.raw"
 
         original_path = os.path.join(original_dir, original_file_name)
         denoised_path = os.path.join(denoised_dir, denoised_file_name)
         
-        original_patches, original_patch_numbers = extract_patches_from_rgb_image(original_path)
-        denoised_patches, denoised_patch_numbers = extract_patches_from_rgb_image(denoised_path)
+        original_patches, original_patch_numbers = extract_y_channel_from_yuv_with_patch_numbers(original_path, row['width'], row['height'])
+        denoised_patches, denoised_patch_numbers = extract_y_channel_from_yuv_with_patch_numbers(denoised_path, row['width'], row['height'])
 
         all_original_patches.extend(original_patches)
         all_denoised_patches.extend(denoised_patches)
@@ -112,7 +114,7 @@ def prepare_data(data, labels):
 
 #########################################################################################################################################################################################################################################
 
-def save_metric_details(model_name, technique, feature_name, test_acc, weighted_precision, weighted_recall, weighted_f1_score, test_loss, accuracy_0, accuracy_1, result_file_path):
+def save_metric_details(model_name, technique, test_acc, weighted_precision, weighted_recall, weighted_f1_score, macro_precision, macro_recall, macro_f1_score, micro_precision, micro_recall, micro_f1_score, test_loss, accuracy_0, accuracy_1, result_file_path):
 
     if path.exists(result_file_path):
     
@@ -120,36 +122,45 @@ def save_metric_details(model_name, technique, feature_name, test_acc, weighted_
         df_new_row = pd.DataFrame({
             'Model': [model_name],
             'Technique' : [technique],
-            'Feature Map' : [feature_name],
             'Overall Accuracy': [test_acc],
-            'Precision': [weighted_precision],
-            'Recall': [weighted_recall],
-            'F1-Score': [weighted_f1_score],
+            'Weight Precision': [weighted_precision],
+            'Weight Recall': [weighted_recall],
+            'Weight F1-Score': [weighted_f1_score],
+            'Macro Precision': [macro_precision],
+            'Macro Recall': [macro_recall],
+            'Macro F1-Score': [macro_f1_score],
+            'Micro Precision': [micro_precision],
+            'Micro Recall': [micro_recall],
+            'Micro F1-Score': [micro_f1_score],
             'Loss': [test_loss],
             'Non-Ghosting Artifacts Accuracy': [accuracy_0],
             'Ghosting Artifacts Accuracy': [accuracy_1]
         })
         df_metrics = pd.concat([df_existing, df_new_row], ignore_index=True)
     else:
-    
+ 
         df_metrics = pd.DataFrame({
             'Model': [model_name],
-            'Technique' : [technique],            
-            'Feature Map' : [feature_name],
+            'Technique' : [technique],
             'Overall Accuracy': [test_acc],
-            'Precision': [weighted_precision],
-            'Recall': [weighted_recall],
-            'F1-Score': [weighted_f1_score],
+            'Weight Precision': [weighted_precision],
+            'Weight Recall': [weighted_recall],
+            'Weight F1-Score': [weighted_f1_score],
+            'Macro Precision': [macro_precision],
+            'Macro Recall': [macro_recall],
+            'Macro F1-Score': [macro_f1_score],
+            'Micro Precision': [micro_precision],
+            'Micro Recall': [micro_recall],
+            'Micro F1-Score': [micro_f1_score],
             'Loss': [test_loss],
             'Non-Ghosting Artifacts Accuracy': [accuracy_0],
             'Ghosting Artifacts Accuracy': [accuracy_1]
         })
 
     df_metrics.to_csv(result_file_path, index=False)
-
 ##########################################################################################################################################################################
 
-def create_vgg16_model(input_shape=(224,224, 3)):
+def create_vgg16_model(input_shape=(224,224, 1)):
     input_layer = Input(shape=input_shape)
   
     x = Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv1')(input_layer)
@@ -189,14 +200,13 @@ def create_vgg16_model(input_shape=(224,224, 3)):
 
 
 ##########################################################################################################################################################################
-print("start")
+
 original_patches, denoised_patches, labels, denoised_image_names, all_patch_numbers = load_data_from_csv(csv_path, original_dir, denoised_dir)
 
 diff_patches = calculate_difference(original_patches, denoised_patches)
 diff_patches_np, labels_np = prepare_data(diff_patches, labels)
 
 print(f" Total Patches: {len(diff_patches_np)}")
-print(f" Total Patches Shape: {diff_patches_np.shape}")
 print(f" Total Labels: {len(labels_np)}")
 
 combined = list(zip(diff_patches_np, labels_np, denoised_image_names, all_patch_numbers))
@@ -236,54 +246,22 @@ opt = Adam(learning_rate=2e-05)
 vgg16_wcw_model = create_vgg16_model()
 vgg16_wcw_model.compile(optimizer=opt, loss='binary_crossentropy', metrics=['accuracy'])
     
-wcw_model_checkpoint = keras.callbacks.ModelCheckpoint(filepath='/WACV_Paper/Models_RAW/VGG16_RGB_wCW.keras', save_best_only=True, monitor='val_accuracy', mode='max', verbose=1)
+wcw_model_checkpoint = keras.callbacks.ModelCheckpoint(filepath='/WACV_Paper/Models_RAW/LOW_VGG16_RAW_wCW.keras', save_best_only=True, monitor='val_accuracy', mode='max', verbose=1)
 wcw_model_early_stopping = keras.callbacks.EarlyStopping(monitor='val_accuracy', min_delta=0, patience=10, restore_best_weights=True)
 
 wcw_history = vgg16_wcw_model.fit(X_train, y_train, epochs=50, validation_data=(X_val, y_val), callbacks=[wcw_model_checkpoint, wcw_model_early_stopping])
 
 wcw_history_df = pd.DataFrame(wcw_history.history)
-wcw_history_df.to_csv('/WACV_Paper/History/VGG16_RGB_wCW.csv', index=False)
+wcw_history_df.to_csv('/WACV_Paper/History_RAW/LOW_VGG16_RAW_wCW.csv', index=False)
 
-
-##########################################################################################################################################################################
-##########################################################################################################################################################################
-                                                            ## With Class Weight
-##########################################################################################################################################################################
-##########################################################################################################################################################################
-
-ng = len(total_patches[total_labels == 0])
-ga =  len(total_patches[total_labels == 1])
-total = ng + ga
-
-imbalance_ratio = ng / ga  
-weight_for_0 = (1 / ng) * (total / 2.0)
-weight_for_1 = (1 / ga) * (total / 2.0)
-class_weight = {0: weight_for_0, 1: weight_for_1}
-
-print('Weight for class 0 (Non-ghosting): {:.2f}'.format(weight_for_0))
-print('Weight for class 1 (Ghosting): {:.2f}'.format(weight_for_1))
-
-opt = Adam(learning_rate=2e-05)
-vgg16_cw_model = create_vgg16_model()
-vgg16_cw_model.compile(optimizer=opt, loss='binary_crossentropy', metrics=['accuracy'])
-
-cw_model_checkpoint = keras.callbacks.ModelCheckpoint(filepath='/WACV_Paper/Models_RAW/VGG16_RGB_CW.keras', save_best_only=True, monitor='val_accuracy', mode='max', verbose=1)
-cw_model_early_stopping = keras.callbacks.EarlyStopping(monitor='val_accuracy', min_delta=0, patience=10, restore_best_weights=True)
-
-cw_history = vgg16_cw_model.fit(X_train, y_train, epochs=50, class_weight=class_weight, validation_data=(X_val, y_val), callbacks=[cw_model_early_stopping, cw_model_checkpoint])
-
-cw_history_df = pd.DataFrame(cw_history.history)
-cw_history_df.to_csv('/WACV_Paper/History/VGG16_RGB_CW.csv', index=False)
 ##########################################################################################################################################################################
 ##########################################################################################################################################################################
                                                                     # Testing
 ##########################################################################################################################################################################
 ##########################################################################################################################################################################
-
-X_test = np.array(X_test)
-
-##########################################################################################################################################################################
 ## Without Class Weight
+##########################################################################################################################################################################
+
 
 test_loss, test_acc = vgg16_wcw_model.evaluate(X_test, y_test)
 test_acc  = test_acc *100
@@ -294,7 +272,7 @@ predicted_labels = np.argmax(predictions, axis=1)
 precision, recall, _ = precision_recall_curve(y_test, predictions[:, 0])
 
 pr_data = pd.DataFrame({'Precision': precision, 'Recall': recall })
-file_path = '/WACV_Paper/Plots_RAW/VGG16_RGB_wCW_PR_Curve.csv'
+file_path = '/WACV_Paper/Plots_CSV/LOW_VGG16_RAW_wCW_PR_Curve.csv'
 pr_data.to_csv(file_path, index=False)
 
 
@@ -305,7 +283,7 @@ plt.ylabel('Precision')
 plt.title('Precision-Recall Curve')
 plt.legend()
 plt.grid(True)
-precision_recall_curve_path = '/WACV_Paper/Plots_RAW/VGG16_RGB_wCW_PR_Curve.png'
+precision_recall_curve_path = '/WACV_Paper/Plots_RAW/LOW_VGG16_RAW_wCW_PR_Curve.png'
 
 if not os.path.exists(os.path.dirname(precision_recall_curve_path)):
     os.makedirs(os.path.dirname(precision_recall_curve_path))
@@ -349,84 +327,40 @@ weighted_f1_score  = weighted_f1_score*100
 weighted_precision = weighted_precision*100
 weighted_recall    = weighted_recall*100
 
+macro_precision = (precision_0 + precision_1) / 2
+macro_recall = (recall_0 + recall_1) / 2
+
+if macro_precision + macro_recall > 0:
+    macro_f1_score = 2 * (macro_precision * macro_recall) / (macro_precision + macro_recall)
+else:
+    macro_f1_score = 0
+  
+macro_f1_score  = macro_f1_score * 100
+macro_precision = macro_precision * 100
+macro_recall    = macro_recall * 100
+
+
+TP_0 = total_class_0 * recall_0
+TP_1 = total_class_1 * recall_1
+FP_0 = total_class_0 * (1 - precision_0)
+FP_1 = total_class_1 * (1 - precision_1)
+FN_0 = total_class_0 * (1 - recall_0)
+FN_1 = total_class_1 * (1 - recall_1)
+
+micro_precision = (TP_0 + TP_1) / (TP_0 + TP_1 + FP_0 + FP_1)
+micro_recall = (TP_0 + TP_1) / (TP_0 + TP_1 + FN_0 + FN_1)
+
+if micro_precision + micro_recall > 0:
+    micro_f1_score = 2 * (micro_precision * micro_recall) / (micro_precision + micro_recall)
+else:
+    micro_f1_score = 0
+
+
+micro_f1_score  = micro_f1_score * 100
+micro_precision = micro_precision * 100
+micro_recall    = micro_recall * 100
 
 model_name = "VGG16"
-feature_name = "Difference Map"
 technique = "Without Class Weight"
-save_metric_details(model_name, technique, feature_name, test_acc, weighted_precision, weighted_recall, weighted_f1_score, test_loss, accuracy_0, accuracy_1, result_file_path)
-print(f"Accuracy: {test_acc:.4f} | precision: {weighted_precision:.4f}, Recall={weighted_recall:.4f}, F1-score={weighted_f1_score:.4f}, Loss={test_loss:.4f}, N.G.A Accuracy={accuracy_0:.4f}, G.A Accuracy={accuracy_1:.4f}")
-
-
-##########################################################################################################################################################################
-
-## With Class Weight
-
-test_loss, test_acc = vgg16_cw_model.evaluate(X_test, y_test)
-test_acc  = test_acc *100
-
-predictions = vgg16_cw_model.predict(X_test)
-predicted_labels = np.argmax(predictions, axis=1)
-
-precision, recall, _ = precision_recall_curve(y_test, predictions[:, 0])
-
-pr_data = pd.DataFrame({'Precision': precision, 'Recall': recall })
-file_path = '/WACV_Paper/Plots_RAW/VGG16_RGB_CW_PR_Curve.csv'
-pr_data.to_csv(file_path, index=False)
-
-plt.figure()
-plt.plot(recall, precision, linestyle='-', color='b')
-plt.xlabel('Recall')
-plt.ylabel('Precision')
-plt.title('Precision-Recall Curve')
-plt.legend()
-plt.grid(True)
-precision_recall_curve_path = '/WACV_Paper/Plots_RAW/VGG16_RGB_CW_PR_Curve.png'
-
-if not os.path.exists(os.path.dirname(precision_recall_curve_path)):
-    os.makedirs(os.path.dirname(precision_recall_curve_path))
-
-plt.savefig(precision_recall_curve_path, dpi=300)
-plt.close()
-
-
-report = classification_report(y_test, predicted_labels, output_dict=True, target_names=["Non-Ghosting Artifact", "Ghosting Artifact"])
-
-conf_matrix = confusion_matrix(y_test, predicted_labels)
-TN = conf_matrix[0, 0]
-FP = conf_matrix[0, 1]
-FN = conf_matrix[1, 0]
-TP = conf_matrix[1, 1]
-
-total_class_0 = TN + FP
-total_class_1 = TP + FN
-correctly_predicted_0 = TN
-correctly_predicted_1 = TP
-
-
-accuracy_0 = (TN / total_class_0) * 100
-accuracy_1 = (TP / total_class_1) * 100
-
-precision_0 = TN / (TN + FN) if (TN + FN) > 0 else 0
-recall_0 = TN / (TN + FP) if (TN + FP) > 0 else 0
-precision_1 = TP / (TP + FP) if (TP + FP) > 0 else 0
-recall_1 = TP / (TP + FN) if (TP + FN) > 0 else 0
-
-
-weighted_precision = (precision_0 * total_class_0 + precision_1 * total_class_1) / (total_class_0 + total_class_1)
-weighted_recall = (recall_0 * total_class_0 + recall_1 * total_class_1) / (total_class_0 + total_class_1)
-
-if weighted_precision + weighted_recall > 0:
-    weighted_f1_score = 2 * (weighted_precision * weighted_recall) / (weighted_precision + weighted_recall)
-else:
-    weighted_f1_score = 0
-
-weighted_f1_score  = weighted_f1_score*100
-weighted_precision = weighted_precision*100
-weighted_recall    = weighted_recall*100
-
-
-model_name = "VGG16"
-feature_name = "Difference Map"
-technique = "Class Weight"
-save_metric_details(model_name, technique, feature_name, test_acc, weighted_precision, weighted_recall, weighted_f1_score, test_loss, accuracy_0, accuracy_1, result_file_path)
-print(f"Accuracy: {test_acc:.4f} | precision: {weighted_precision:.4f}, Recall={weighted_recall:.4f}, F1-score={weighted_f1_score:.4f}, Loss={test_loss:.4f}, N.G.A Accuracy={accuracy_0:.4f}, G.A Accuracy={accuracy_1:.4f}")
+save_metric_details(model_name, technique, test_acc, weighted_precision, weighted_recall, weighted_f1_score, macro_precision, macro_recall, macro_f1_score, micro_precision, micro_recall, micro_f1_score, test_loss, accuracy_0, accuracy_1, result_file_path)
+print(f"Accuracy: {test_acc:.4f} | precision: {weighted_precision:.4f}, Recall={weighted_recall:.4f}, F1-score={weighted_f1_score:.4f}, Macro Precision: {macro_precision:.4f}, Macro Recall={macro_recall:.4f}, Macro F1-score={macro_f1_score:.4f}, Micro precision: {micro_precision:.4f}, Micro Recall={micro_recall:.4f}, Micro F1-score={micro_f1_score:.4f}, Loss={test_loss:.4f}, N.G.A Accuracy={accuracy_0:.4f}, G.A Accuracy={accuracy_1:.4f}")
